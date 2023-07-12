1767649697c330feefbd7878b339f1d3bd30c0b1
sangeles <sangeles@anscom.net>
2023-07-11 19:41:47 -0600

    establecimientos parcial

--- app/Http/Controllers/EstablecimientoController.php
+++ app/Http/Controllers/EstablecimientoController.php
@@ -21,12 +21,77 @@ class EstablecimientoController extends Controller
 
     public function store(Request $request) 
     {
-        $request->validate([
-    		'nombreEstablecimiento' => 'required',
-    	]);
+        $validData = $request->validate(
+            ['nombreEstablecimiento' => 'required'],
+            ['nombreEstablecimiento.required' => 'El campo NOMBRE DEL ESTABLECIMIENTO es requerido'],
+            ['slogan' => 'required'],
+            ['slogan.required' => 'El campo SLOGAN es requerido'],
+            ['logo' => 'required'],
+            ['logo.required' => 'El campo NOMBRE DEL ESTABLECIMIENTO es requerido'],
+            ['calleNumero' => 'required'],
+            ['calleNumero.required' => 'El campo NOMBRE DEL ESTABLECIMIENTO es requerido'],
+            ['colonia' => 'required'],
+            ['colonia.required' => 'El campo NOMBRE DEL ESTABLECIMIENTO es requerido'],
+            ['municipio' => 'required'],
+            ['municipio.required' => 'El campo MUNICIPIO O ALCALDÍA es requerido'],
+            ['cp' => 'required'],
+            ['cp.required' => 'El campo CÓDIGO POSTAL es requerido'],
+            ['telefono1' => 'required'],
+            ['telefono1.required' => 'El campo TELÉFONO 1 es requerido'],
+            ['encargado' => 'required'],
+            ['encargado.required' => 'El campo NOMBRE DEL ENCARGADO es requerido'],
+            ['email' => 'required'],
+            ['email.required' => 'El campo CORREO ELECTRÓNICO DEL ENCARGADO es requerido']);
+  
+        
+
+        
+    		
+
 
         $establecimiento = $request->user()->establecimientos()->create([
             'nombreEstablecimiento' =>   $request->nombreEstablecimiento,
+            
+            'slogan' =>   $request->slogan,
+            'logo' =>   $request->logo,
+            'calleNumero' =>   $request->calleNumero,
+            'colonia' =>   $request->colonia,
+            'municipio' =>   $request->municipio,
+            'cp' =>   $request->cp,
+            'estado' =>   $request->estado,
+            'telefono1' =>   $request->telefono1,
+            'telefono2' =>   $request->telefono2,
+            'encargado' =>   $request->encargado,
+            'eMail' =>   $request->email,
+            'sitioWeb' =>   $request->nombreEstablecimiento, 
+            'faceBook' =>   $request->faceBook,
+            'instagram' =>   $request->instagram,
+            'twitter' =>   $request->twitter,
+            'tikTok' =>   $request->tikTok,
+            'youtube' =>   $request->youtube,
+            'status' =>   $request->status,
+            'lunesEstablecimiento' =>   $request->lunesEstablecimiento,
+            'horaInicioLunesEstablecimiento' =>   $request->horaInicioLunesEstablecimiento,
+            'horaFinLunesEstablecimiento' =>   $request->horaFinLunesEstablecimiento,
+            'martesEstablecimiento' =>   $request->martesEstablecimiento,
+            'horaInicioMartesEstablecimiento' =>   $request->horaInicioMartesEstablecimiento,
+            'horaFinMartesEstablecimiento' =>   $request->horaFinMartesEstablecimiento,
+            'miercolesEstablecimiento' =>   $request->miercolesEstablecimiento,
+            'horaInicioMiercolesEstablecimiento' =>   $request->horaInicioMiercolesEstablecimiento,
+            'horaFinMiercolesEstablecimiento' =>   $request->horaFinMiercolesEstablecimiento,
+            'juevesEstablecimiento' =>   $request->nombreEstablecimiento,
+            'horaInicioJuevesEstablecimiento' =>   $request->nombreEstablecimiento,
+            'horaFinJuevesEstablecimiento' =>   $request->nombreEstablecimiento,
+            'viernesEstablecimiento' =>   $request->nombreEstablecimiento,
+            'horaInicioViernesEstablecimiento' =>   $request->nombreEstablecimiento,
+            'horaFinViernesEstablecimiento' =>   $request->nombreEstablecimiento,
+            'sabadoEstablecimiento' =>   $request->nombreEstablecimiento,
+            'horaInicioSabadoEstablecimiento' =>   $request->nombreEstablecimiento,
+            'horaFinSabadoEstablecimiento' =>   $request->nombreEstablecimiento,
+            'domingoEstablecimiento' =>   $request->nombreEstablecimiento,
+            'horaInicioDomingoEstablecimiento' =>   $request->nombreEstablecimiento,
+            'horaFinDomingoEstablecimiento' =>   $request->nombreEstablecimiento,
+    
         ]);
 
         return view('establecimientos.index', [
